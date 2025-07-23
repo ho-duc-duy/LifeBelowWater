c4b37e80a6523ac2e61e22402e9640e9b45a8079
ho-duc-duy <duychanc6705@gmail.com>
2025-07-23 10:11:03 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 //Released under GPL3. more information: https://www.gnu.org/licenses/gpl-3.0.en.html
-//Inspirational references:
+//References for inspo:
 //Interactive Ripple Grid – CodePen by VoXelo – https://codepen.io/VoXelo/pen/LEYaMbJ
 //“p5.js Spiral Wave Pattern” – YouTube video URL: https://www.youtube.com/watch?v=zR5onrdWCeY
 
